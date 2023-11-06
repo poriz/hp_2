22069850697aaa9de24fb4f8253f13b7517a72ed
yiReal <andytwo9878@gmail.com>
2023-11-07 07:16:36 +0900

    Set DB
    models.py - 필요 필드 생성
    views.py - 임시로 작성. 후에 올바르게 수정 필요
    migrations - models.py로 migrate

--- hp_2site/main/models.py
+++ hp_2site/main/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class AREA_INFO(models.Model):
+    AREA_CD = models.CharField(max_length = 10, primary_key = True)
+    AREA_NM = models.CharField(max_length = 255)
+    AREA_CONGEST_LVL = models.CharField(max_length = 100)
+    TEMP = models.FloatField()
+    PM10 = models.FloatField()
+    PM25 = models.FloatField()
+    CATEGORY = models.CharField(max_length=50, default='분류오류')  # 카테고리 필드 추가
+    def select_category(self):
+        if int(self.AREA_CD[-3:]) >= 1 and int(self.AREA_CD[-3:]) <= 7:
+            return '관광특구'
+        
+        elif int(self.AREA_CD[-3:]) >= 8 and int(self.AREA_CD[-3:]) <= 12:
+            return '고궁·문화유산'
+        
+        elif int(self.AREA_CD[-3:]) >= 13 and int(self.AREA_CD[-3:]) <= 56:
+            return '인구밀집지역'
+        
+        elif int(self.AREA_CD[-3:]) >= 57 and int(self.AREA_CD[-3:]) <= 84:
+            return '발달상권'
+        
+        elif int(self.AREA_CD[-3:]) >= 85 and int(self.AREA_CD[-3:]) <= 113:
+            return '공원'
+        
+        else:
+            return '분류오류'
+    def save(self, *args, **kwargs):
+        self.CATEGORY = self.select_category()  # 카테고리를 설정
+        super(AREA_INFO, self).save(*args, **kwargs)  # 부모 클래스의 save 메서드 호출
--- hp_2site/main/views.py
+++ hp_2site/main/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from .models import AREA_INFO
 
 # Create your views here.
 def index(request):
     msg = '테스트 메세지'
     return render(request,'main/index.html',{'message':msg})
+
+def data_to_db(request):
+    # API에서 데이터 가져오기
+    # data 에 API에서 딕셔너리화한 데이터를 반환
+    # API 가져오는 함수 붙여넣으면 될 것 같습니다!
+    datas = {}
+
+    # 데이터베이스에 저장
+    for data in datas:
+        area_info = AREA_INFO(**data)
+        area_info.save()
+
+    return HttpResponse("Data saved")
