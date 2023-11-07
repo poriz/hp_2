b6b3cfacb1d116be86276adc66ac081654dfbf97
lyh <mediwind@gmail.com>
2023-11-07 18:44:07 +0900

    very_samll_fix

--- hp_2site/main/views.py
+++ hp_2site/main/views.py
@@ -10,12 +10,6 @@ from .models import *
 import requests
 import xml.etree.ElementTree as ET
 
-# from selenium import webdriver
-# from selenium.webdriver.chrome.service import Service
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.ui import WebDriverWait
-# from webdriver_manager.chrome import ChromeDriverManager
-
 import pandas as pd
 
 # read env
