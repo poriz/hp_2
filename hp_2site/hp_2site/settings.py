d3ae6e679f7006cb06639f8627d2b4c35ce75b90
poriz <areacmzl@gmail.com>
2023-11-07 21:05:38 +0900

    cron update

--- hp_2site/hp_2site/settings.py
+++ hp_2site/hp_2site/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'rest_framework',
     'main.apps.MainConfig',
+    'django_apscheduler',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +125,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
